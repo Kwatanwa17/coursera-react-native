709271c28d03b04576ac4d530fbf4930266fd2f8
Kwatanwa17 <kwatanwa17@gmail.com>
2020-04-06 17:30:24 +0200

    Navigation Part 2

--- components/MainComponent.js
+++ components/MainComponent.js
@@ -1,40 +1,83 @@
 import React, {Component} from "react";
+import Home from "./HomeComponent";
 import Menu from './MenuComponent';
 import Dishdetail from './DishdetailComponent';
 import {View, Platform} from 'react-native';
 import {NavigationContainer} from '@react-navigation/native';
 import {createStackNavigator} from '@react-navigation/stack';
+import {createDrawerNavigator} from '@react-navigation/drawer';
 import Constants from 'expo-constants';
 
 const Stack = createStackNavigator();
 
 function MenuNavigator() {
     return (
-        <NavigationContainer>
-            <Stack.Navigator
-                screenOptions={{
-                    initialRouteName: 'Menu',
-                    navigationOptions: {
-                        headerStyle: {
-                            backgroundColor: '#512DA8'
-                        },
-                        headerTintColor: '#fff',
-                        headerTitleStyle: {
-                            color: '#fff'
-                        }
+
+        <Stack.Navigator
+            screenOptions={{
+                initialRouteName: 'Menu',
+                navigationOptions: {
+                    headerStyle: {
+                        backgroundColor: '#512DA8'
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        color: '#fff'
                     }
-                }}>
-                <Stack.Screen
-                    name="Menu"
-                    component={Menu}
-                    options={{title: 'Menu'}}
-                />
-                <Stack.Screen
-                    name="Dishdetail"
-                    component={Dishdetail}
-                    options={{title: 'Dishdetail'}}
-                />
-            </Stack.Navigator>
+                }
+            }}>
+            <Stack.Screen
+                name="Menu"
+                component={Menu}
+                options={{title: 'Menu'}}
+            />
+            <Stack.Screen
+                name="Dishdetail"
+                component={Dishdetail}
+                options={{title: 'Dishdetail'}}
+            />
+        </Stack.Navigator>
+
+    );
+}
+
+function HomeNavigator() {
+    return (
+
+        <Stack.Navigator
+            screenOptions={{
+                navigationOptions: {
+                    headerStyle: {
+                        backgroundColor: '#512DA8'
+                    },
+                    headerTintColor: '#fff',
+                    headerTitleStyle: {
+                        color: '#fff'
+                    }
+                }
+            }}>
+            <Stack.Screen
+                name="Home"
+                component={Home}
+                options={{title: 'Home'}}
+            />
+        </Stack.Navigator>
+
+    );
+}
+
+const Drawer = createDrawerNavigator();
+
+function MainNavigator() {
+    return (
+        <NavigationContainer>
+            <Drawer.Navigator initialRouteName="Home"
+                              drawerStyle={{
+                                  backgroundColor: '#D1C4E9'
+                              }}>
+                <Drawer.Screen name="Home" component={HomeNavigator}/>
+                <Drawer.Screen name="Menu" component={MenuNavigator}/>
+            </Drawer.Navigator>
         </NavigationContainer>
     );
 }
@@ -44,7 +87,7 @@ class Main extends Component {
     render() {
         return (
             <View style={{flex: 1, paddingTop: Platform.OS === 'ios' ? 0 : Constants.statusBarHeight}}>
-                <MenuNavigator/>
+                <MainNavigator/>
             </View>
         );
     }
